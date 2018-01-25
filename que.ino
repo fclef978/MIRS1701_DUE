cef34c2edaf345f55c1078944ac39ee17f21f9d3
fclef978 <fclef978@icloud.com>
2018-01-25 17:50:36 +0900

    キュー完成(仮)

--- que.ino
+++ que.ino
@@ -1,33 +1,34 @@
-static volatile unsigned int readPointer = 0;
-static volatile unsigned int writePointer = 0;
+static volatile unsigned char readPointer = 0;
+static volatile unsigned char writePointer = 0;
 
 static int bufSize = 256;
-static volatile int q[256];
+static volatile int q[256][8];
+
 
 void queOpen() {
   readPointer = 0;
   writePointer = 0;
-  for (int i = 0; i < bufSize; i++) {
-    q[i] = 0;
+  for (int i = 0; i < 256; i++) {
+    for (int j = 0; j < 8; j++) {
+      q[i][j] = 0;
+    }
   }
 }
 
-void quePush(int num) {
-  unsigned char wp = writePointer & 0xFF;
-  q[wp] = num;
-  writePointer++;
+void quePush(int cmd[]) {
+  int wp = writePointer++;
+  for (int i = 0; i < 8; i++) {
+    q[wp][i] = cmd[i];
+  }
 }
 
-int quePop() {
-  unsigned char rp = readPointer & 0xFF;
-  int result = q[rp];
-  readPointer++;
-  return result;
+void quePop(int cmd[]) {
+  int rp = readPointer++;
+  for (int i = 0; i < 8; i++) {
+    cmd[i] = q[rp][i];
+  }
 }
 
 int queGetLength() {
-  unsigned char wp = writePointer & 0xFF;
-  unsigned char rp = readPointer & 0xFF;
-  return wp - rp;
+  return writePointer - readPointer;
 }
-
